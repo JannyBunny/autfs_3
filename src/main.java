240f8a0f1e887e21fe0be9a08381dbd8584c808c
Jan <gruenhagen@strebergarten.org>
2017-06-26 21:31:02 +0200

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,58 @@
+import java.util.Stack;
+
+/**
+ * 
+ */
+
+/**
+ * @author jan
+ *
+ */
+public class main implements I_SpecificParser {
+
+	/**
+	 * 
+	 */
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void startLLParser(String wort) throws ParseException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void S(Character left, Stack zuErsetzen) throws ParseException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void T(Character left, Stack zuErsetzen) throws ParseException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void X(Character left, Stack zuErsetzen) throws ParseException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Y(Character left, Stack zuErsetzen) throws ParseException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
